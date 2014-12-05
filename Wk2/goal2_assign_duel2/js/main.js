654f48f0f0ee9c56bf07986f36b357e3469c721d
joannahogan <joannahogan@joannas-mbp.attlocal.net>
2014-12-05 15:30:43 -0500

    Arrays in Duel
    Added flowchart for search engine

--- Wk2/goal2_assign_duel2/js/main.js
+++ Wk2/goal2_assign_duel2/js/main.js
@@ -1,47 +1,38 @@
-/******** Joanna Hogan 12/1/14 ANALYZE Duel #1 ********
+/******************************************
+ Week 2 Assignment: Develop Duel #2
+ Joanna Hogan       05 DEC 14
+ *******************************************/
 
- * Duel Fight Game - FINISHED
- * Date: 4/09/13
-
- Assignment 1
- Part 1/3 of series
-*/
 
 // self-executing function
 (function(){ // Start of function
 
     console.log("FIGHT!!!");  // Display "Fight!!!" to console
 
-    //player name
-    var playerOneName = "Spiderman";  // Declare variable for player one
-    var playerTwoName = "Batman";  // Declare variable for player two
 
-    //player damage
-    var player1Damage = 20;  // Declare variable for damage to player one
-    var player2Damage = 20;  // Declare variable for damage to player two
+    //Replace variables for each player with an array declaring name, damage, & health
+    var playerOne = ["Spiderman", 20, 100];
+    var playerTwo = ["Batman", 20, 100];
 
-    //player health
-    var playerOneHealth = 100;  // Declare variable for health player one
-    var playerTwoHealth = 100;  // Declare variable for health player two
 
     //initiate round
     var round=0;  // Declare variable round to equal zero
 
     function fight(){
-        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth); // Display alert with names and starting health
+        alert(playerOne[0]+":"+playerOne[2]+"  *START*  "+playerTwo[0]+":"+playerTwo[2]); // Display alert with names and starting health
         for (var i = 0; i < 10; i++) // For loop with conditions
         {
             //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = player1Damage * .5; // Declare variable minDamage1 to be the result of the expression
-            var minDamage2 = player2Damage * .5; // Declare variable minDamage2 to be the result of the expression
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1); // Calculate player 1 damage
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2); // Calculate player 2 damage
+            var minDamage1 = playerOne[1] * .5; // Declare variable minDamage1 to be the result of the expression
+            var minDamage2 = playerTwo[1] * .5; // Declare variable minDamage2 to be the result of the expression
+            var f1 = Math.floor(Math.random()*(playerOne[1]-minDamage1)+minDamage1); // Calculate player 1 damage
+            var f2 = Math.floor(Math.random()*(playerTwo[1]-minDamage2)+minDamage2); // Calculate player 2 damage
 
             //inflict damage
-            playerOneHealth-=f1; // Decreases player 1 health by f1 calculation
-            playerTwoHealth-=f2;  // Decreases player 2 health by f2 calculation
+            playerOne[2]-=f1; // Decreases player 1 health by f1 calculation
+            playerTwo[2]-=f2;  // Decreases player 2 health by f2 calculation
 
-            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);  // Display to console player names and health status
+            console.log(playerOne[0]+": "+playerOne[2] + " " + playerTwo[0]+":"+playerTwo[2]);  // Display to console player names and health status
 
             //check for victor
             var result = winnerCheck();  // Function winnerCheck calculates resuld
@@ -49,7 +40,7 @@
             if (result==="no winner")  // If statement to continue game if no winner
             {
                 round++; // Round incrementally increased by one
-                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth); // Alert to user current health at the end of the round
+                alert(playerOne[0]+":"+playerOne[2]+"  *ROUND "+round+" OVER"+"*  "+playerTwo[0]+":"+playerTwo[2]); // Alert to user current health at the end of the round
 
             } else{  // Else statement to alert user of winner and end game
                 alert(result);  // Alert to user the result
@@ -61,14 +52,14 @@
 
     function winnerCheck(){  // Function to calculate if there is a winner
         var result="no winner";  // Result is string "no winner"
-        if (playerOneHealth<1 && playerTwoHealth<1) // If statement for both players losing
+        if (playerOne[2]<1 && playerTwo[2]<1) // If statement for both players losing
         {
             result = "You Both Die";  // Both players losing results in string of "You both die"
-        } else if(playerOneHealth<1){ // Else if statement if player 2 wins
-            result =playerTwoName+" WINS!!!"  // Result is Batman wins
-        } else if (playerTwoHealth<1)  // Else if statement if player 1 wins
+        } else if(playerOne[2]<1){ // Else if statement if player 2 wins
+            result =playerTwo[0]+" WINS!!!"  // Result is Batman wins
+        } else if (playerTwo[2]<1)  // Else if statement if player 1 wins
         {
-            result = playerOneName+" WINS!!!"  // Result is Spiderman wins
+            result = playerOne[0]+" WINS!!!"  // Result is Spiderman wins
         };
        return result; // Return result to previous code
     };
